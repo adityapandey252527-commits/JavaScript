6732bbc2cac826b49f2dd9bc916b2b7f369bd912
Aditya Pandey <adityapandey252527@gmail.com>
2026-07-05 20:35:46 +0000

    DataTypes

---
+++ 01_basics/01_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodeJS, not browser
+
+console.log(3+
+    3) // code readablity shuld be high 
+    
+console.log('Aditya Pandey')
+
+let name = "aditya"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint 
+// string ="" ''
+// boolean => true/false
+// null => stand alone value
+// undefined => not defined value
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined);
+
+console.log(typeof null);
