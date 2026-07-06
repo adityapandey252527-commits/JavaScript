327be5952bf894bbad8303a027c1908a569e3e52
Aditya Pandey <adityapandey252527@gmail.com>
2026-07-06 18:40:09 +0000

    Conversion and Operation

--- 01_basics/Conversion.js
+++ 01_basics/Conversion.js
@@ -1,29 +1,75 @@
-let score = false
+// let score = false
 
-console.log(score)
-console.log(typeof score)
+// console.log(score)
+// console.log(typeof score)
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // undefined => NaN
 // "33" => 33
 // "33abc" => NaN
 // true => 1 ; false =>0
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 = true ; 0 = false
 // "Aditya" => true
 
-let somenumber = 33
+// let somenumber = 33
 
-let stringNumber = String(somenumber)
+// let stringNumber = String(somenumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//********** Operations ***********//
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2%3)
+
+let str1 = "Hello"
+let str2 = " Aditya"
+let str3 = str1+str2
+
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(3+4*5%3);
+
+
+// console.log(+true);
+// console.log(+"")
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2
+// console.log(num1)
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter)
+
+let x =100
+let a = x++
+console.log(x)
+console.log(a)
+console.log(x)
+++x;
+console.log(x)
+
