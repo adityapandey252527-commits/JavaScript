630d8ac20448e35b9ef66dbde3678cc508e7bddc
Aditya Pandey <adityapandey252527@gmail.com>
2026-07-06 18:19:18 +0000

    Conversion Of datatypes

---
+++ 01_basics/Conversion.js
@@ -0,0 +1,29 @@
+let score = false
+
+console.log(score)
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// undefined => NaN
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false =>0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 = true ; 0 = false
+// "Aditya" => true
+
+let somenumber = 33
+
+let stringNumber = String(somenumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
