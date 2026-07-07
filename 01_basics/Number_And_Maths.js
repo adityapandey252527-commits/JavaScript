60148a7775749fcb3c5e4bfce1a428f29174ae63
Aditya Pandey <adityapandey252527@gmail.com>
2026-07-07 18:10:27 +0000

    Number and Maths

---
+++ 01_basics/Number_And_Maths.js
@@ -0,0 +1,50 @@
+// const score = 400
+// console.log(score)
+
+// const balance  = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+// const otherNumber = 123.8967
+// console.log(otherNumber.toPrecision(3))
+
+// const hudreds = 10000000
+// console.log(hudreds.toLocaleString())
+
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.EPSILON)
+
+
+
+/****************************************  MATHS  *************************************************************************/
+// console.log(Math)
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3))
+// console.log(Math.round(4.6))
+
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.2))
+
+// console.log(Math.floor(4.8))
+// console.log(Math.ceil(4.8))
+
+// let num1 = Math.min(2,3,1,8)
+// console.log(num1)
+
+// let num2 = Math.max(2,3,1,8)
+// console.log(num2)
+
+console.log(Math.random())
+console.log((Math.random()*10)+1 )
+console.log(Math.floor(Math.random()*10)+1 )
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min)
