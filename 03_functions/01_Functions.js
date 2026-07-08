fbbb6d93f17252ed0595a84cb2609f26d80c6568
Aditya Pandey <adityapandey252527@gmail.com>
2026-07-08 18:49:09 +0000

    Function

---
+++ 03_functions/01_Functions.js
@@ -0,0 +1,24 @@
+
+function sayMyname(){
+    console.log("A")
+    console.log("D")
+    console.log("I")
+    console.log("T")
+    console.log("Y")
+    console.log("A")
+
+}
+// sayMyname()
+function AddTwoNumber(number1, number2){
+    console.log(number1+number2);
+}
+
+function AddTwoNumber(number1, number2){
+//    let result = number1+number2
+//    return result
+   
+    return number1+number2
+}
+
+const result =AddTwoNumber(3, 5)
+console.log("Result:", result)  //undefined
