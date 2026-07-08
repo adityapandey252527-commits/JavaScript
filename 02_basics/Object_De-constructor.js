6b28c7adf911387ca85b2365eb2d4495c273f3c2
Aditya Pandey <adityapandey252527@gmail.com>
2026-07-08 18:30:59 +0000

    Object De-Constructor

---
+++ 02_basics/Object_De-constructor.js
@@ -0,0 +1,22 @@
+const course = {
+    courseName: "JavaScript",
+    price : "999",
+    courseInstructor: "Aditya"
+}
+
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor);
+console.log(instructor)
+
+// {
+//     "name": "Aditya",
+//     "courseName": "JavaScipt",
+//     "price": "Free"
+// }
+
+
+[
+    {},
+    {},
+    {},
+]
