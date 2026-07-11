950424f8fb2788f6853cd23cb7bcff64db55f2c7
Aditya Pandey <adityapandey252527@gmail.com>
2026-07-11 18:27:47 +0000

    IIFE

---
+++ 03_functions/05_IIFE.js
@@ -0,0 +1,10 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function chai(){
+    // named iife
+    console.log(`DB Connected`);
+})();
+
+( (name)=> {
+    console.log(`DB Connected Two ${ name}`);
+}) ('Aditya')
