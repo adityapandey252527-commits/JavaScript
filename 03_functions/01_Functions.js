9fd16d0f33cf4f414a6537868f691f73cb141381
Aditya Pandey <adityapandey252527@gmail.com>
2026-07-11 11:28:46 +0000

    Functions

--- 03_functions/01_Functions.js
+++ 03_functions/01_Functions.js
@@ -1,4 +1,3 @@
-
 function sayMyname(){
     console.log("A")
     console.log("D")
@@ -9,16 +8,46 @@ function sayMyname(){
 
 }
 // sayMyname()
-function AddTwoNumber(number1, number2){
-    console.log(number1+number2);
-}
+
+// function AddTwoNumber(number1, number2){
+//     console.log(number1+number2);
+// }
+
+// const result =AddTwoNumber(3, 5)
+// console.log("Result:", result)  //undefined
 
 function AddTwoNumber(number1, number2){
-//    let result = number1+number2
-//    return result
+   let result = number1+number2
+   return result
    
-    return number1+number2
+    // return number1+number2
 }
 
-const result =AddTwoNumber(3, 5)
-console.log("Result:", result)  //undefined
+// const result =AddTwoNumber(3, 5)
+// console.log("Result:", result) 
+
+// console.log("Result:", AddTwoNumber(3, 5))
+
+
+
+// function loggedInMessage(username){
+//     if(username === undefined){
+//         console.log("Please enter a user name");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+// // console.log(loggedInMessage("Aditya"))
+// console.log(loggedInMessage())
+
+
+
+function loggedInMessage(username="Sammy"){
+    if(!username){
+        console.log("Please enter a user name");
+        return
+    }
+    return `${username} just logged in`
+}
+// console.log(loggedInMessage("Aditya"))
+console.log(loggedInMessage())
