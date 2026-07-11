c4672126b06558759822f18cf8fbe0eec8783df6
Aditya Pandey <adityapandey252527@gmail.com>
2026-07-11 11:49:23 +0000

    function object

---
+++ 03_functions/02_Functions_with_object.js
@@ -0,0 +1,48 @@
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+// console.log(calculateCartPrice(200,400,500,5000,340,2098))
+
+// const { use } = require("react");
+
+// function calculateCartPrice(val1, val2, ...num1){
+//     return num1
+// }
+// console.log(calculateCartPrice(200,400,500,5000,340,2098))
+
+// const user = {
+//     username: "Aditya",
+//     price: 199
+// }
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+// }
+// handleObject(user);
+
+
+
+// const user = {
+//     username: "Aditya",
+//     price: 199
+// }
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+
+// const myNewArray =[200,400,100,600]
+// function returnSecondValue(getArray){
+//     return getArray[1]
+// }
+// console.log(returnSecondValue(myNewArray));
+
+
+const myNewArray =[200,400,100,600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue([200,900,349,560]));
