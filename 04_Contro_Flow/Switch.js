d11a7ddc24b7d084df98d2edbbdb100641bdb198
Aditya Pandey <adityapandey252527@gmail.com>
2026-07-12 18:14:36 +0000

    Switch

---
+++ 04_Contro_Flow/Switch.js
@@ -0,0 +1,54 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+//     case 6:
+//         console.log("june");
+//         break;
+//     case 7:
+//         console.log("july");
+//         break;
+//     default:
+//         console.log("Default case match")
+//         break;
+// }
+
+const month = "march"
+switch (month) {
+    case "january":
+        console.log("january");
+        break;
+    case "february":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+        console.log("Default case match")
+        break;
+}
