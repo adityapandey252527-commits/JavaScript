22410771e4cebad7746e8e16ba98bf2981f8f3bc
Aditya Pandey <adityapandey252527@gmail.com>
2026-07-12 19:59:58 +0000

    for in loop

---
+++ 05_Iterations/four.js
@@ -0,0 +1,36 @@
+const myObj ={
+    js: "javascript",
+    cpp: "c++",
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+// for (const key in myObj) {
+//     console.log(key);
+    
+// }
+// for (const key in myObj) {
+//     console.log(myObj[key]);
+    
+// }
+
+for (const key in myObj) {
+    // console.log(`${key} shortcut is for ${myObj[key]}`);
+    
+}
+
+const programming = ["js", 'rb', "py", "java", "cpp"]
+for (const key in programming) {
+    // console.log(programming[key])
+    
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('US', "United State Of America")
+// map.set('FR', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(map[key])
+// }
+
