60fc2e5ccd8f55f1fc2a4a2a41047c17eaf1032c
Aditya Pandey <adityapandey252527@gmail.com>
2026-07-26 14:15:38 +0000

    ProtoType

---
+++ 11_ClassesAndObject/Prototype.js
@@ -0,0 +1,21 @@
+let myName = "Aditya     "
+let myChannel = "Chai"
+
+// console.log(myName.length);
+// console.log(myName.trim().length);
+
+// console.log(myName.truelength());
+
+let myHero = ["thor", "spiderman"]
+
+let heroPower = {
+    thor: "Hammer",
+    spiderman: "Sling",
+
+    getSpiderPower : function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+
+heroPower.Aditya()
