8a8ee868db7bcc0bfc60c9b07ad62c946516e76c
Aditya Pandey <adityapandey252527@gmail.com>
2026-08-07 07:56:41 +0000

    Call

--- 11_ClassesAndObject/call.js
+++ 11_ClassesAndObject/call.js
@@ -1,10 +1,25 @@
+// function SetUsername(username){
+//         // complex DB calls
+//     this.username = username
+//     console.log("Called");
+// }
+// function createUser(username, email, password){
+//     SetUsername(username)
+    
+//     this.email = email
+//     this.password = password
+// }
+// const chai = new createUser("Aditya", "aditya@gmail.com",'123')
+// console.log(chai)
+
+
 function SetUsername(username){
         // complex DB calls
     this.username = username
     console.log("Called");
 }
 function createUser(username, email, password){
-    SetUsername.call(this, username)
+    SetUsername.call(this, username)  //for referance hold  .call  (this, username)  using this
     
     this.email = email
     this.password = password
