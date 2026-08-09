a7b2ba1d11a325db00d65e23fffd738bec2f18e0
Aditya Pandey <adityapandey252527@gmail.com>
2026-08-09 08:01:24 +0000

    Class get And set

---
+++ 11_ClassesAndObject/GetterAndSetter.js
@@ -0,0 +1,23 @@
+class User{
+    constructor(email, password){
+        this.email = email;
+        this.password  = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value.toUpperCase()
+    }
+
+    get password(){
+        return `${this._password.toUpperCase()}`
+    }
+    set password(value){
+        this._password = value.toUpperCase()
+    }
+}
+
+const hitesh = new User("aditya@gmail.com", "abc")
+console.log(hitesh.password);
